e7197342b796d1800134f22167941905141feb9e
decilya <decilya@gmail.com>
2017-03-24 12:10:59 +0300

    Created new migrate user

--- config/db.php
+++ config/db.php
@@ -1,9 +1,18 @@
 <?php
 
+/*
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
     'password' => '',
+    'charset' => 'utf8',
+];*/
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2-blog-test',
+    'username' => 'root',
+    'password' => 'qwerty',
     'charset' => 'utf8',
 ];
