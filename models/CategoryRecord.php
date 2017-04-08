463520f6714d6c7acfa58b29b32ade5162196b21
decilya <decilya@gmail.com>
2017-04-08 19:36:24 +0300

    Tested GRUD Generator

---
+++ models/CategoryRecord.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "category".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property integer $created_at
+ */
+class CategoryRecord extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'category';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['created_at'], 'integer'],
+            [['name'], 'string', 'max' => 250],
+            [['name'], 'unique'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'created_at' => 'Created At',
+        ];
+    }
+}
